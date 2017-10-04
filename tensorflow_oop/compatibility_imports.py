05e77902c3fdf08d424b2225223cbf87815e34d9
Ravil Gataullin <ravil.gataullin@kaspersky.com>
2017-10-04 14:10:47 +0300

    Add compatibility imports file.

---
+++ tensorflow_oop/compatibility_imports.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+from __future__ import division
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+from future.utils import raise_from
+from future.utils import with_metaclass
+
+from builtins import int
+from builtins import bytes
+from builtins import str
+from builtins import dict
+from builtins import object
+from builtins import range
+from builtins import map
+from builtins import input
+from builtins import chr
+
+from io import BytesIO
+from io import StringIO
+from io import open
+
+from functools import reduce
+
+from imp import reload
